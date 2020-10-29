733a52ddc4a5c1b9cf61053ceb4f367144904543
hxrxchang <m.y.y.0326.cg@gmail.com>
2020-10-29 17:12:55 +0900

    不要なrequireを削除

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
-require 'redis'
-Rails.application.config.redis_a = Redis.new(host: ENV["REDIS_HOST"], port: 6380, db: 0)
-Rails.application.config.redis_b = Redis.new(host: ENV["REDIS_HOST"], port: 6381, db: 0)
+Rails.application.config.redis_a = Redis.new(host: ENV["REDIS_HOST"], port: 6380)
+Rails.application.config.redis_b = Redis.new(host: ENV["REDIS_HOST"], port: 6381)
